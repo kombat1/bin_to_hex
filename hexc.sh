b00781ce9d4964570a2933f5d80e04eba6aefd82
kombat <kolyakovalev2000@mail.ru>
2019-12-10 05:19:15 -0500

    Update hexc.sh

--- hexc.sh
+++ hexc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-for i in `odjump -d $1 example | tr '\t' ' ' | tr ' ' '\n' | egrep'^[0-9a-f]{2}$'`; do echo -n "\x$i";
+for i in `odjump -d $1 | tr '\t' ' ' | tr ' ' '\n' | egrep'^[0-9a-f]{2}$'`; do echo -n "\x$i";
