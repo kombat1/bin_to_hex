2b0bcb9716e5c027200e1b7426f2e0a784e88496
kombat <kolyakovalev2000@mail.ru>
2020-08-18 05:50:22 -0400

    Update hexc.sh

--- hexc.sh
+++ hexc.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-
-for i in `odjump -d $1 | tr '\t' ' ' | tr ' ' '\n' | egrep'^[0-9a-f]{2}$'`; do echo -n "\x$i";
+for i in `objdump -d $1 | tr '\t' ' ' | tr ' ' '\n' | egrep '^[0-9a-f]{2}$' `; do echo -n "\x$i"; done; printf "\n"
